ddb4f79637f634da363baf13f35ea1512b710cfd
Ole Magnus Johnsen <ole.magnus@me.com>
2022-01-30 01:37:42 +0100

    profile picture on index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import Header from '../components/Header';
 import SEO from '../components/SEO';
+import Image from 'next/image';
+import profilepic from '../public/images/profilepic.png';
 
 const Home = () => {
     return (
         <>
             <SEO title="OMFJ - Home" />
             <Header title="Ole Magnus" />
+            <div className="
+                flex
+                justify-center
+                mb-5
+            ">
+                <Image 
+                    src={profilepic}
+                    width={125}
+                    height={125}
+                    className="
+                        rounded-full
+                    "
+                />
+            </div>
             <p className="
                 font-bold
                 text-center
